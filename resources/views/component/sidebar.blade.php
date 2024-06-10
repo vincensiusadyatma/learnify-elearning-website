472475091423e314a00f37547e9ed45c43f8ad85
Nicolaus Reva <nicolaussagraha14@gmail.com>
2024-06-10 22:56:28 +0700

    add sidebar

---
+++ resources/views/component/sidebar.blade.php
@@ -0,0 +1,48 @@
+        <!--start: sidebar-->
+        <aside : class="left-0 top-0 z-9999 flex h-[100%] w-72 flex-col bg-blue-200 duration-300 ease-linear bg-gray-900">
+            <!--start: sidebar header-->
+            <div class="flex items-center gap-5 px-6 py-5 lg:py-6.5">
+            <img src="src/css/logo learnify.png" alt="learnify logo" class="object-contain w-[20%]">
+            <h1 class="text-white text-3xl font-bold"">Learnify</h1>
+            </div>
+            <!--end : sidebar header-->
+
+            <div class="no-scrollbar flex flex-col justify-between overflow-y-auto duration-300 ease-linear">
+                <!--start: sidebar menu-->
+                    <nav class="mt-5 py-4 px-4 lg:mt-9 lg:px-6 flex flex-col justify-evenly">
+                        <!--start: menu group-->
+                        <div>
+                            <h2 class="mb-4 ml-4 text-sm font-medium text-white text-opacity-50">MENU</h2>
+                            <ul class="mb-6 flex flex-col gap-1.5">
+                                <li>
+                                <a href="#" class="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-white hover:bg-gray-700 rounded focus:outline-none focus:ring focus:ring-gray-200">
+                                    <img src="public/img/toga.png"  alt="toga image" class="object-contain w-[20%] h-auto">
+                                    <h3 class="text-white">Dashboard</h3>
+                                </a>
+                                </li>
+
+                                <li>
+                                    <a href="#" class="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-white hover:bg-gray-700 rounded focus:outline-none focus:ring focus:ring-gray-200">
+                                        <img src="public/img/course2.png" alt="course image" class="object-contain w-[20%] h-auto">
+                                        <h3 class="text-white">Course</h3>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!--end: menu group-->
+                    </nav>
+
+                    <div class="mt-5 py-4 px-4 lg:px-6 lg:mt-9">
+                        <ul class="mb-6 flex flex-col gap-1.5">
+                            <li>
+                                <a href="#" class="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-white hover:bg-gray-700 rounded focus:outline-none focus:ring focus:ring-gray-200">
+                                    <img src="public/img/logout.png" alt="course image" class="object-contain w-[20%] h-auto">
+                                    <h3 class="text-white">Course</h3>
+                                </a>
+                                </li>
+                            </ul>
+                    </div>
+                <!--end: sidebar menu-->
+            </div>
+        </aside>
+        <!--end: sidebar-->
